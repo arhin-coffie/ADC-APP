4cdfb1fd63b7851ccdcd51ea2b5a994d519853f1
nyankson-benjamin <117347759+nyankson-benjamin@users.noreply.github.com>
2024-01-20 20:07:14 +0000

    feature: add members card

---
+++ src/components/UserCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="w-60 h-64  border rounded-lg flex items-center flex-col shadow-sm">
+        <div>
+            <img src="https://cdn.modernghana.com/images/content/c28jbn4ayt_2ndc_1.png" alt="profile-image"
+                class="w-32 h-32 rounded-full p-3 shadow-md flex items-start justify-start" />
+        </div>
+
+        <div class="mt-5">
+            <p>John Doe</p>
+            <p>Regional Chairman</p>
+            <p>National Chairman</p>
+            <p>Joh Doe</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
