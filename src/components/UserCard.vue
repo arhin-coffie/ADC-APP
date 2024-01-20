b82a4b417840ac80caa4589f33b86c74b9416990
nyankson-benjamin <117271631+benjamin-nyankson@users.noreply.github.com>
2024-01-20 20:22:24 +0000

    feat: hover effect

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-60 h-64  border rounded-lg flex items-center flex-col shadow-sm">
+    <div class="w-60 h-64  border rounded-lg flex items-center flex-col shadow-sm hover:shadow-lg">
         <div>
             <img src="https://cdn.modernghana.com/images/content/c28jbn4ayt_2ndc_1.png" alt="profile-image"
                 class="w-32 h-32 rounded-full p-3 shadow-md flex items-start justify-start" />
